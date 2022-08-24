6fea97b73e0ca91e50e210d5c228af11dbc75165
Petelin Anna <s2200743@BC-C02J61CNQ6LR-MacBook-Air.local>
2022-08-24 12:44:02 +0300

    calculator task, click event listener not working

--- week3/calculators/main.js
+++ week3/calculators/main.js
@@ -1,15 +1,26 @@
 
 
-function gasCalculation(price, money){
-let litres = money/price;
-return litres;
+function outcomeDisplay() {
+    let outcome = document.getElementById("outcome"); //<p> element
+    let price = Number(document.getElementById("priceInput").value);
+    console.log("price input:", price);
+    let money = Number(document.getElementById("moneyInput").value);
+    console.log("money input: ", money);
 
+    let litres = Math.floor(money / price);
+    console.log(litres);
+
+
+    if (litres >= 10) {
+        outcome.textContent = `You can get about ${litres} litres. Good, now you can escape.`
+    }
+    else {
+        outcome.textContent = `You can only get about ${litres} litres. You will have to stay here.`
+    }
 }
 
-let price = getElementById("priceInput");
-let money = getElementById("moneyInput");
-let litres =  gasCalculation(price,money);
-let outcome = getElementById("outcome");
-getElementById();
-if (litres >= 10){outcome.textContent = `Good, you can get about ${litres} litres.`
-}
+
+let calculateButton = document.getElementById("calculateButton");
+calculateButton.addEventListener("click", outcomeDisplay());
+
+// getElement finds the element, but not its value. You need to get the attribute called 'value' of the input object. By default it is an empty string. 
