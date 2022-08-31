786e1f15b64bdca6f6159a73c021cd2b9f0c02c1
Anna Petelin <andorjamb@protonmail.com>
2022-08-31 14:44:47 +0300

    end of lessons wed 2108

--- week3/calculators/insurance/main.js
+++ week3/calculators/insurance/main.js
@@ -5,40 +5,84 @@ for each pre-existing health condition, increase of 1% cost
 for each good habit, reduce price by 5% per good habit
 bad habits - increase 5% per bad habit
 */
-
-console.log("aghhhh");
+const form = document.querySelector("form");
 const client = document.querySelector("#client");
-const age = document.getElementById("age").value;
+//console.log('client', client.value);
+function ageLoading() {
+  let age = document.querySelector("#age").value;
+  let ageCategory;
+  let penalty = 1;
 
-console.log(client.value);
-console.log(age);
+  if (age < 19) {
+    ageCategory = "A";
+  } else if (18 > age || age < 26) {
+    ageCategory = "B";
+    penalty = 1.1;
+  } else if (25 > age || age < 36) {
+    ageCategory = "C";
+    penalty = 1.3;
+  } else if (35 > age || age < 46) {
+    ageCategory = "D";
+    penalty = 1.6;
+  } else if (45 > age || age < 56) {
+    ageCategory = "E";
+    penalty = 2;
+  } else if (55 > age || age < 66) {
+    ageCategory = "F";
+    penalty = 2.5;
+  } else if (age > 65) {
+    ageCategory = "G";
+    penalty = 3.2;
+  }
 
-function ageSurcharge(age) {
-    let penalty = 1;
-    switch (age) {
-        case age < 18:
-            penalty = 1;
-            break;
-        case age < 26:
-            penalty = 1.1;
-            break;
-        case age < 36:
-            penalty = 1.3;
-            break;
-        case age < 46:
-            penalty = 1.6;
-            break;
-        case age < 56:
-            penalty = 2;
-            break;
-        case age < 66:
-            penalty = 2.5;
-            break;
-        case age > 65:
-            penalty = 3.1;
-            break;
-    }
-    return penalty * 500;
+  let basePremium = penalty * 500;
+
+  const ageOutput = document.getElementById("ageOutput");
+  ageOutput.textContent = `Your age increases your base premium to ${basePremium}€.`;
+  ageOutput.style.border = "2px solid rgba(48, 48, 110, 0.331)";
+  ageOutput.style.borderRadius = "17px";
+  return basePremium;
 }
 
-console.log(ageSurcharge(age));
+function healthPenalty() {
+  let healthConditions = 0; // maybe this should be global so it isn't reset on function trigger
+  let healthPremium = 0;
+  let healthOptions = document.getElementsByName("health");
+  healthOptions.forEach(function (item) {
+    if (item.checked) {
+      //healthConditions = healthConditions + 1;
+      healthPremium = basePremium * 1.01;
+    }
+    console.log(healthConditions);
+  });
+
+  /*increase of 1% cost for each*/
+  const healthOutput = document.getElementById("healthOutput");
+  healthOutput.textContent = `Your health conditions increase your premium to ${healthPremium}€.`;
+  return healthPremium;
+}
+
+function habitPenalty() {
+  console.log(smoking);
+}
+
+function calculateAll() {
+  console.log(ageLoading(age));
+  console.log(healthForm);
+}
+
+const hypertension = document.getElementById("hypertension");
+const bloodsugar = document.getElementById("bloodsugar");
+const overweight = document.getElementById("overweight");
+hypertension.addEventListener("change", healthPenalty);
+bloodsugar.addEventListener("change", healthPenalty);
+overweight.addEventListener("change", healthPenalty);
+
+const smoking = document.getElementById("smoking");
+const alcohol = document.getElementById("alcohol");
+const drugs = document.getElementById("drugs");
+smoking.addEventListener("change", habitPenalty);
+alcohol.addEventListener("change", habitPenalty);
+drugs.addEventListener("change", habitPenalty);
+
+form.addEventListener("change", ageLoading);
