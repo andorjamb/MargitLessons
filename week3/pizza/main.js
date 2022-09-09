aa2d922213308a7297b5b82fe17d356a05ebeee1
Petelin Anna <s2200743@BC-C02J61CNQ6LR-MacBook-Air.local>
2022-09-09 20:31:07 +0300

    javascript still not working

--- week3/pizza/main.js
+++ week3/pizza/main.js
@@ -16,35 +16,38 @@ toppings.forEach((item)=>{toppingsResult.push(item.value);})
 
 
 let total = 0; // variable for total order cost
-const pizzaForm = document.querySelector('pizzaOrder');
 
+const pizzaForm = document.querySelector("pizzaForm");
 const toppings = document.getElementsByName("toppings");
-console.log(toppings);
-const sizes = document.getElementsByName("size");
 
-pizzaForm.addEventListener('submit', readOrder);
-console.log("toppings array contents:", toppingsList);
 
-const form = document.querySelector("form");
-form.addEventListener("submit", test);
+
+pizzaForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log("print me to show I am listening");
+});
+
+pizzaForm.addEventListener('change', readOrder);
+/* console.log("toppings array contents:", toppingsList);
+ */
+
+
 
 function readOrder() {
-    test();
+
     const cost = function () {
+        const sizes = document.getElementsByName("sizes");
+        sizes.forEach(function (size) { if (size) { } })
         let price = Number(sizes.value);
         return price;
     }
+    console.log('price is', price)
     total = total + cost;
     console.log(`total is ${total}`);
 
 
 }
 
-function test(event) {
-    event.preventDefault();
-    console.log("print me when something is being changed, to show I am listening");
-
-}
 
 let toppingSelection = function () {
     const toppingsList = []; // an empty array of strings
