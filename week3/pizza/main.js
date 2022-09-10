eab4b435d8c8971fc1e6153e1eeeb273e4e32703
Petelin Anna <s2200743@BC-C02J61CNQ6LR-MacBook-Air.local>
2022-09-10 10:37:24 +0300

    pizza subfunctions now working

--- week3/pizza/main.js
+++ week3/pizza/main.js
@@ -1,70 +1,67 @@
-/*
-////////Notes from Margit's example://///////
-
-const form = document.querySelector('form');
-const size = document.querySelectorAll();
-const toppings = document.querySelectorAll();
-const takeOrder = function(event){
-event.preventDefault(); 
-let customerName = customer.value;  
-};
-
-form.addEventListener('submit', takeOrder)
-
-toppings.forEach((item)=>{toppingsResult.push(item.value);})
-*/
-
 
 let total = 0; // variable for total order cost
-
-const pizzaForm = document.querySelector("pizzaForm");
-const toppings = document.getElementsByName("toppings");
-
-
+const pizzaForm = document.getElementById("pizzaForm");
 
 pizzaForm.addEventListener("submit", function (event) {
     event.preventDefault();
-    console.log("print me to show I am listening");
 });
 
-pizzaForm.addEventListener('change', readOrder);
-/* console.log("toppings array contents:", toppingsList);
- */
+pizzaForm.addEventListener('submit', readOrder);
 
-
-
-function readOrder() {
-
-    const cost = function () {
-        const sizes = document.getElementsByName("sizes");
-        sizes.forEach(function (size) { if (size) { } })
-        let price = Number(sizes.value);
-        return price;
-    }
-    console.log('price is', price)
-    total = total + cost;
-    console.log(`total is ${total}`);
-
-
+function getSize() {
+    const sizes = document.getElementsByName("sizes");
+    sizes.forEach(function (size) { if (size.checked) { sizePrice = Number(size.value); } })
+    return sizePrice;
 }
+//error when undefined
 
-
-let toppingSelection = function () {
-    const toppingsList = []; // an empty array of strings
+function getToppings() {
+    const toppingsList = [];
+    const toppings = document.getElementsByName("toppings");
     toppings.forEach(function (topping) {
         if (topping.checked) {
-            console.log(topping.value);
             toppingsList.push(topping.value)
         };
     })
     return toppingsList;
 }
-
-
-function summary() {
-    const customer = document.querySelector('customer');
-    nameField = document.querySelector('nameField');
-    nameField.textContent = customer.value;
-    console.log('customer name: ', customer.value);
+function getDelivery() {
+    let deliveryChoice = document.getElementById("delivery");
+    console.log(deliveryChoice.value);
+    if (deliveryChoice.value == "delivery") {
+        return parseFloat(5);
+    }
+    else { return 0 }
 }
+
+function getCustomer() {
+    let customer = document.getElementById("customer").value;
+    console.log('customer name:', customer);
+    return customer;
+}
+
+function readOrder() {
+    let sizePrice = getSize(); // a number
+    let toppingSelection = getToppings();  //a string array
+    let deliveryCost = getDelivery(); // number
+    let customer = getCustomer(); // string
+
+    function summary() {
+        let nameField = document.getElementById('nameField');
+        let sizeField = document.getElementById('sizeField');
+        let toppingsField = document.getElementById('toppingsField');
+        let deliveryField = document.getElementById('deliveryField');
+        nameField.textContent = customer;
+        sizeField.textContent = sizePrice;
+        toppingsField.textContent = toppingSelection;
+        deliveryField.textContent = deliveryCost;
+    }
+
+
+
+    total = sizePrice + extraToppings + deliveryCost;
+    console.log(`total is ${total}`);
+}
+
+
 
