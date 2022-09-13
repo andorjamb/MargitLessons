a59adc81d43349eca71e87ef9c9b3e22f6236cbb
Petelin Anna <s2200743@BC-C02J61CNQ6LR-MacBook-Air.local>
2022-09-13 22:38:48 +0300

    cars database, search function added but not working

--- week4/cars_database/main.js
+++ week4/cars_database/main.js
@@ -5,16 +5,17 @@ let cars = [];
 
 const addCar = document.querySelector("#addCar"); //button
 const reset = document.querySelector("#reset");
+const searchButton = document.querySelector("#searchButton"); // search button
 const carTable = document.querySelector('#carTable');
 form.addEventListener('reset', function () {
     cars = [];
 });
+searchButton.addEventListener("submit", findCar);
 form.addEventListener("submit", function (event) {
     event.preventDefault();
     cars.push(getCarData());
     let newCarValues = Object.values(getCarData());
     populateTable(newCarValues);
-
 })
 
 //constructor function for the car object
@@ -37,21 +38,32 @@ function getCarData() { //function collects values from input fields
     const carPrice = document.querySelector("#carPrice").value;
     const carColor = document.querySelector("#carColor").value;
     return newCar = new Car(carLicence, carMaker, carModel, carOwner, carPrice, carColor);
-
 }
 
 function populateTable(valueArray) {
     let newRow = document.createElement('tr');
+    newRow.className = "database-container-body";
     carTable.appendChild(newRow);
     for (let j = 0; j < valueArray.length; j++) {//for each car object value:
         let newData = document.createElement('td');
+        newData.className = "table-cells";
         newData.innerHTML = valueArray[j];
         carTable.appendChild(newData);
-
     }
 }
 
-    //syntax: for(const i of carArray)){}
-    //search button can be a submit button
+function findCar() {
+    const searchResult = document.getElementById("searchResult");
+    const searchLicence = document.getElementById("searchLicence");
+    console.log(searchLicence);
+    for (const k of cars) {
+        if (cars[k].licenceNumber === searchLicence) {
+            searchResult.textContent = `Result: Car owner: ${cars[k].carOwner}, car Model: ${cars[k].carMaker}, car make: ${cars[k].carModel}`;
+            return cars[k];
+        } else { searchResult.textContent = "The car was not found." }
+    }
+}
+
+
 
 
