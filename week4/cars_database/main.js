f1143537535d7dcb04e7e37472e8937e563ecfd0
Petelin Anna <s2200743@BC-C02J61CNQ6LR-MacBook-Air.local>
2022-09-13 11:26:28 +0300

    cars database task js add object from input working

--- week4/cars_database/main.js
+++ week4/cars_database/main.js
@@ -1,27 +1,42 @@
 
 const form = document.querySelector("form");
-const carMaker = document.querySelector("#carMaker");
-const carModel = document.querySelector("#carModel");
-const carOwner = document.querySelector("#carOwner");
-const carPrice = document.querySelector("#carPrice");
-const carColor = document.querySelector("#carColor");
+let newCar;
+let cars = []; //empty array to contain each car object added to database
+
+const addCar = document.querySelector("#addCar");
+const reset = document.querySelector("#reset");
 
 form.addEventListener("submit", function (event) {
-    event.preventDefault;
-    getCarData();
+    event.preventDefault();
+    populateTable(getCarData());
+
 })
 
 //constructor function for the car object
-function Car(licence, maker, model, owner, price, color) {
-    this.licenceNumber = licence;
-    this.carMaker = maker;
-    this.carModel = model;
-    this.carOwner = owner;
-    this.carPrice = price;
-    this.carColor = color;
+class Car {
+    constructor(licence, maker, model, owner, price, color) {
+        this.licenceNumber = licence;
+        this.carMaker = maker;
+        this.carModel = model;
+        this.carOwner = owner;
+        this.carPrice = price;
+        this.carColor = color;
+    }
 }
 
-function getCarData() {
-    const newCar = new Car();
+function getCarData() { //function collects values from input fields
+    const carLicence = document.querySelector("#carLicence").value;
+    const carMaker = document.querySelector("#carMaker").value;
+    const carModel = document.querySelector("#carModel").value;
+    const carOwner = document.querySelector("#carOwner").value;
+    const carPrice = document.querySelector("#carPrice").value;
+    const carColor = document.querySelector("#carColor").value;
+    return newCar = new Car(carLicence, carMaker, carModel, carOwner, carPrice, carColor);
 
+}
+
+function populateTable() {  //function prints new object to table 'database'
+    //console.table(array or object); each value in the array will a <td> in the row
+    let newRow = getCarData();
+    console.table(newRow);
 }
