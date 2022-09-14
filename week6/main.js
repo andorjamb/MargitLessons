3f64080033f26b9adb746ade250d6555c6268d45
Petelin Anna <s2200743@BC-C02J61CNQ6LR-MacBook-Air.local>
2022-09-14 14:58:48 +0300

    end of lesson JS margit week6

--- week6/main.js
+++ week6/main.js
@@ -1,8 +1,46 @@
 
 
-const slidingMenu = document.getElementByClassName("slidingMenu");
+const slidingMenu = document.getElementById("slidingMenu");
+const navbar = document.querySelector('nav');
 const backToTop = document.getElementById("backToTop");
 const header = document.querySelector("header");
 header.addEventListener('scroll', function () { header.classList.toggle("") })
-backToTop.addEventListener('click', function () { window.scroll(0, 0) });
+backToTop.addEventListener('click', function () { window.scroll(0, 0) }); /* or window.scrollTop = 0 */
 
+//scrollTop built-in property
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+const scrollFunction = function () { /*NOT WORKING*/
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        header.classList.add('scroll');
+        backToTop.style.display = 'block'; /*works more reliably than the visibility property*/
+    } else {
+        header.classList.remove('scroll');
+        backToTop.style.display = 'none';
+    }
+}
+
+const doNotClick = document.getElementById("doNotClick");
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close');
+doNotClick.addEventListener('click', function () {
+    overlay.style.display = 'flex';
+    modal.style.display = 'block';
+    close.style.display = 'block';
+    /*disable scroll*/
+})
+
+const menuIcon = document.getElementById('menuIcon');
+menuIcon.addEventListener('click', function () {
+    if (navbar.classList.contains('responsive')) {
+        navbar.classList.remove('responsive');
+        document.body.style.overflow = '';
+    } else {
+        navbar.classList.add('responsive');
+        document.body.style.overflow = 'hidden';
+    }
+})
