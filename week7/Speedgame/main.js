e6ccbbc948e9290005a4d3ad4f85b6cd859e4bb8
Anna Petelin <adpetelin@gmail.com>
2022-09-21 15:01:20 +0300

    end of lesson week7

--- week7/Speedgame/main.js
+++ week7/Speedgame/main.js
@@ -1,34 +1,74 @@
+/* ///////   DOCUMENT OBJECTS     //////////// */
+
 const startButton = document.querySelector("#startButton");
 const stopButton = document.querySelector("#stopButton");
-const scoreDisplay = document.querySelector('#scoreDisplay');
+const scoreDisplay = document.getElementById('scoreDisplay');
 const circle = document.querySelectorAll(".circle");
 const active = document.querySelector(".active");
 
+
+/* ////////////  GLOBAL VARIABLES //////////  */
 let score = 0;
+let newSeed, lastSeed;
+
+
+
+/* ////////////  FUNCTION DEFINITIONS //////////  */
 
 function startGame() {
     console.log('game started');
-
+    active = checkRandom();
 }
 
+function checkRandom() {
+    active = generateRandom();
+    if (active != lastActive) {
+        lastActive = active;
+        return active;
+    }
+    else { generateRandom() }
+}
 function stopGame() {
     console.log('game ended.');
 }
 
 circle.forEach((node, index) => {
     node.addEventListener('click', () => {
-        registerCircle(node)
-        console.log(node);
-        score++;
+        showIndex(index);
         console.log('score is: ', score);
+        return score;
+
     });
 })
 
+function showIndex(index) {
+    console.log('clicked on circle', index);
+    score++;
+    scoreDisplay.textContent = `Your score: ${score}`;   //why doesn't this work outside of the function?
+    return score;
+}
 
-function registerCircle() {
+function generateRandom() {
+    newRandom = Math.floor(Math.random() * 4);
+    console.log(newRandom);
+    return newRandom;
 }
 
 
+
+
+/* //////////////  Program execution     /////////////// */
 startButton.addEventListener('click', startGame);
 stopButton.addEventListener('click', stopGame);
-scoreDisplay.textContent = `Your score: ${score}`;
+
+
+
+/* Pseudocode notes 
+
+while(game running) {
+call setTimeout function (calls random number function)
+}
+
+each time new random number generated this value is passed to function that changes class of selected div
+
+*/
