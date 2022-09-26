b2c3b8231e25bcc773723ccc01dca472ac2c43c0
Anna Petelin <andorjamb@protonmail.com>
2022-09-26 18:31:22 +0300

    linear gradient working nicely, CSS needs tweaking

--- week4/background_gradient/main.js
+++ week4/background_gradient/main.js
@@ -1,19 +1,46 @@
+/*  GLOBAL OBJECTS AND VARIABLES */
 
-const direction = document.querySelector(); //get input direction from button
-const background = document.querySelector('body').style.backgroundImage = lineargradient();
-//to right
-//to left
-//to bottom
-//to top
-//to top right
-//to top left
-//to bottom left
-//to bottom right
+const colors = document.querySelectorAll(".colors");
+let color1 = document.querySelector("#color1").value;
+let color2 = document.querySelector("#color2").value;
+const arrows = document.querySelectorAll(".arrow-radio");
+let direction = 0;
 
-console.log(color1);
+/*  SUBFUNCTIONS */
 
-function getGradient() {
-    let color1 = document.querySelector("#color1");
-    let color2 = document.querySelector("#color2");
+function updateDirection(value) {
+  /* WORKING */
+  direction = Number(value);
+  document.querySelector(
+    "body"
+  ).style.backgroundImage = `linear-gradient(${direction}deg, ${color1}, ${color2})`;
 }
 
+function updateColors() {
+  color1 = document.querySelector("#color1").value;
+  color2 = document.querySelector("#color2").value;
+  document.querySelector(
+    "body"
+  ).style.backgroundImage = `linear-gradient(${Number(
+    0
+  )}deg, ${color1}, ${color2})`;
+}
+
+/*  MAIN FUNCTION */
+
+function main() {
+  arrows.forEach((arrow) =>
+    arrow.addEventListener("change", function () {
+      updateDirection(arrow.id);
+    })
+  );
+
+  colors.forEach((color) =>
+    color.addEventListener("change", function () {
+      console.log("logging color on change", color.value);
+      updateColors();
+    })
+  );
+}
+
+window.onload = main();
