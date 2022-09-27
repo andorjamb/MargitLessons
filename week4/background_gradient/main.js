2c7bf8e7e8329f2be498d2c4b1c3e9fa2cef64c8
Anna Petelin <andorjamb@protonmail.com>
2022-09-27 10:34:22 +0300

    added CSS code paragraph and copy function  to linear gradient, all working, ready

--- week4/background_gradient/main.js
+++ week4/background_gradient/main.js
@@ -4,16 +4,18 @@ const colors = document.querySelectorAll(".colors");
 let color1 = document.querySelector("#color1").value;
 let color2 = document.querySelector("#color2").value;
 const arrows = document.querySelectorAll(".arrow-radio");
+const cssCode = document.getElementById("cssCode");
 let direction = 0;
+const copy = document.getElementById("copy");
 
 /*  SUBFUNCTIONS */
 
 function updateDirection(value) {
-  /* WORKING */
   direction = Number(value);
   document.querySelector(
     "body"
   ).style.backgroundImage = `linear-gradient(${direction}deg, ${color1}, ${color2})`;
+  cssCode.textContent = document.querySelector("body").style.backgroundImage;
 }
 
 function updateColors() {
@@ -24,6 +26,7 @@ function updateColors() {
   ).style.backgroundImage = `linear-gradient(${Number(
     0
   )}deg, ${color1}, ${color2})`;
+  cssCode.textContent = document.querySelector("body").style.backgroundImage;
 }
 
 /*  MAIN FUNCTION */
@@ -37,10 +40,13 @@ function main() {
 
   colors.forEach((color) =>
     color.addEventListener("change", function () {
-      console.log("logging color on change", color.value);
       updateColors();
     })
   );
+
+  copy.addEventListener("click", function () {
+    navigator.clipboard.writeText(cssCode.textContent);
+  });
 }
 
 window.onload = main();
