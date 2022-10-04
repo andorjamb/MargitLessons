7ec7be2aa07998ca8525a78b9b89cad1e16c9f3b
Anna Petelin <adpetelin@gmail.com>
2022-10-05 00:28:24 +0300

    cannot get images to work, problem with style ref

--- week7/Speedgame/main.js
+++ week7/Speedgame/main.js
@@ -4,7 +4,6 @@ const startButton = document.querySelector("#startButton");
 const stopButton = document.querySelector("#stopButton");
 const scoreDisplay = document.getElementById('scoreDisplay');
 const circles = document.querySelectorAll(".circle");
-const active = document.querySelector(".active");
 const close = document.querySelector('.close');
 const overlay = document.querySelector('.overlay');
 const modal = document.querySelector('.modal');
@@ -15,23 +14,27 @@ const finalScoreText = document.querySelector("#finalScoreText");
 let score = 0;
 let newCurrent;
 let current = generateRandom();
-const interval = 1500;
+let interval = 1500;
 let alienTimeout;
 
 /* ////////////  FUNCTION DEFINITIONS //////////  */
 
 function startGame() {
     console.log('game started');
-    setTimeout(activateAlien, 1500);
+    alienTimeout = setTimeout(activateAlien, 1500);
 }
 
 function finalScore(score) {
     gameOverText.textContent = `Your score is: ${score}.`;
-    if (score > 20) { finalScoreText.textContent = `Wowzers, you're amazing, and wayyy better at this than Margit`; }
+    if (score > 20) { finalScoreText.textContent = `Wowzers, you're better than Margit`; }
     else if (score > 10) { finalScoreText.textContent = `Pretty good, for an amateur`; }
     else { finalScoreText.textContent = `Man you suck.`; }
 }
 
+function generateRandom() {
+    newRandom = Math.floor(Math.random() * 4);
+    return newRandom;
+}
 function makeRandom() {
     newCurrent = generateRandom();
     if (newCurrent != current) {
@@ -48,45 +51,54 @@ function stopGame() {
     showModal();
 }
 
-function showScore(index) {
-    score++;
+function showScore() {
+    score += 1;
     scoreDisplay.textContent = `Your score: ${score}`;
     return score;
 }
 
-function generateRandom() {
-    newRandom = Math.floor(Math.random() * 4);
-    return newRandom;
-}
 
 function getImage() {
-    console.log(circles[current]);
-    console.log(circles[current].style.backgroundImage);
-    const image1 = "url('./assets/images/planet1_alien1.png')";
-    const image2 = "url('./assets/images/planet1_alien2.png')";
-    const image3 = "url('./assets/images/planet1_alien3.png')";
-    const image4 = "url('./assets/images/planet1_alien4.png')";
+    const image1 = 'url("./assets/images/planet1_alien1.png")';
+    const image2 = 'url("./assets/images/planet1_alien2.png")';
+    const image3 = 'url("./assets/images/planet1_alien3.png")';
+    const image4 = 'url("./assets/images/planet1_alien4.png")';
     const imageArray = [image1, image2, image3, image4];
-    return imageArray[generateRandom()];
+    let imgToReturn = imageArray[generateRandom()]
+    console.log(imgToReturn);
+    return imgToReturn; // returns a URL attribute
 }
 
 function activateAlien() { //the main game function
-    circles[current].style.backgroundImage = "url('./assets/images/planet1.png')";
-    current = makeRandom();
-    circles[current].style.backgroundImage = getImage();
-    circles.forEach((node, index) => {
+    let active = makeRandom();
+    circles[active].style.backgroundImage = getImage();
+    //let selectedClass = 'alien' + (generateRandom() + 1).toString();
+    //console.log(selectedClass);
+    //circles[planetIndex].classList.add(selectedClass);
+    //circles[active].classList.add('alien');
+
+    circles.forEach((node) => {
         node.addEventListener('click', () => {
-            if (!(node === circles[current])) {
+            console.log('circle clicked: ', node);
+            console.log('alien: ', circles[active]);
+            if (!(node == circles[active])) {
+                clearTimeout(alienTimeout);
                 stopGame();
             }
             else {
-                showScore(index); //gets current score
+                node.style.backgroundImage = 'url("assets/images/planet1.png")';
+                if ((score != 0) && (score % 3 == 0)) {
+                    interval -= 100;
+                    console.log('decreasing interval!')
+                };
+                alienTimeout = setTimeout(activateAlien, interval);
+
+                showScore(); //gets current score
                 return score; //being returned from nested function
             }
         })
     })
-    if ((score != 0) && (score % 5 == 0)) { interval = interval - 100 };
-    alienTimeout = setTimeout(activateAlien, interval);
+    current = active;
     return current;
 }
 
@@ -115,5 +127,8 @@ close.addEventListener('click', function () {
 BUGS:
 
 alien background image not working
-
+background image doesn't swap out when game ends
+(monsters stay)
+the last timeout doesn't stop thought the game has ended?
+the click event and monster generaton are out of sync
 */
