0ab7a58548cbb8e9fd54cd2092661c1dc85e6d16
Anna Petelin <adpetelin@gmail.com>
2022-10-04 16:52:01 +0300

    add functions to speedgame

--- week7/Speedgame/main.js
+++ week7/Speedgame/main.js
@@ -5,30 +5,49 @@ const stopButton = document.querySelector("#stopButton");
 const scoreDisplay = document.getElementById('scoreDisplay');
 const circle = document.querySelectorAll(".circle");
 const active = document.querySelector(".active");
-
+const close = document.querySelector('.close');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const gameOverText = document.querySelector("#gameOverText");
+const finalScoreText = document.querySelector("#finalScoreText");
 
 /* ////////////  GLOBAL VARIABLES //////////  */
 let score = 0;
-let seed; 
-let lastSeed = generateRandom;
+let seed;
+let lastSeed = generateRandom();
 
 /* ////////////  FUNCTION DEFINITIONS //////////  */
 
 function startGame() {
     console.log('game started');
-    seed = checkRandom();
+    seed = makeRandom();
 }
 
-function checkRandom() {
+function finalScore(score) {
+    gameOverText.textContent = `Your score is: ${score}.`;
+    if (score > 20) { finalScoreText.textContent = `Wowzers, you're amazing`; }
+    else if (score > 10) { finalScoreText.textContent = `Pretty good, for an amateur`; }
+    else { finalScoreText.textContent = `Man you suck.`; }
+}
+
+function makeRandom() {
     seed = generateRandom();
-    if (seed != lastseed) {
-        lastseed = seed;
+    if (seed != lastSeed) {
+        lastSeed = seed;
         return seed;
     }
     else { generateRandom() }
 }
 function stopGame() {
     console.log('game ended.');
+    finalScore(score);
+    showModal();
+}
+
+function alien() {
+    let divInPlay = circle[makeRandom()];
+    console.log(divInPlay);
+    divInPlay.style.backgroundImage = getImage();
 }
 
 circle.forEach((node, index) => {
@@ -43,7 +62,7 @@ circle.forEach((node, index) => {
 function showIndex(index) {
     console.log('clicked on circle', index);
     score++;
-    scoreDisplay.textContent = `Your score: ${score}`;   //why doesn't this work outside of the function?
+    scoreDisplay.textContent = `Your score: ${score}`;
     return score;
 }
 
@@ -53,10 +72,31 @@ function generateRandom() {
     return newRandom;
 }
 
+function getImage() {
 
+    return imageArray[generateRandom()];
+}
 
+/* modal menu and close button functions */
 
-/* //////////////  Program execution     /////////////// */
+function showModal() {
+    const close = document.querySelector('.close');
+    if (!(overlay.classList.contains('visible'))) {
+        const close = document.querySelector('.close');
+        overlay.classList.add('visible');
+        modal.classList.add('visible');
+        close.classList.add('visible');
+    }
+}
+
+close.addEventListener('click', function () {
+    overlay.classList.remove('visible');
+
+})
+
+/* //////////////  Program execution  and event listeners   /////////////// */
+console.log('print last seed variable: ', lastSeed);
+
 startButton.addEventListener('click', startGame);
 stopButton.addEventListener('click', stopGame);
 
@@ -64,9 +104,7 @@ stopButton.addEventListener('click', stopGame);
 
 /* Pseudocode notes 
 
-while(game running) {
-call setTimeout function (calls random number function)
-}
+
 
 each time new random number generated this value is passed to function that changes class of selected div
 
