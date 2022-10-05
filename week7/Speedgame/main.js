86e008b136351c15489e53de762015282e2ae5a1
andor <andorjamb@protonmail.com>
2022-10-05 21:38:06 +0300

    speedgame on the road to recovery, active selection working

--- week7/Speedgame/main.js
+++ week7/Speedgame/main.js
@@ -12,56 +12,56 @@ const finalScoreText = document.querySelector("#finalScoreText");
 
 /* ////////////  GLOBAL VARIABLES //////////  */
 let score = 0;
-//let newCurrent;
-let current = generateRandom();
-let interval = 1500;
-let alienTimeout;
 let active;
+let alienTimeout;
 
 /* ////////////  FUNCTION DEFINITIONS //////////  */
 
-function startGame() {
+function runGame() {
     console.log('game started');
+    startButton.style.display = "none";
+    stopButton.style.display = "block";
+    activateAlien();
 
-    active = verifyRandom();
-    /*     circles[active].style.backgroundImage = getImage(); */
-    console.log(circles[active])
-    circles[active].style.backgroundImage = 'url("assets/images/planet1_alien4.png")';
-    alienTimeout = setTimeout(activateAlien, 1500);
-
-    circles.forEach((circle) => {
-        circle.addEventListener('click', () => {
-            if (!(circle == circles[active])) {
-                clearTimeout(alienTimeout);
-                stopGame();
-            }
-            else {
-                circle.style.backgroundImage = 'url("assets/images/planet1.png")';
-                if ((score != 0) && (score % 3 == 0)) {
-                    interval -= 100;
-                    console.log('decreasing interval!');
-
-                };
-                showScore();
-                return score;
-            }
-
-        })
-        current = active;
-        //return score;
-        return current;
-        alienTimeout = setTimeout(activateAlien, 1500);
-
+circles.forEach((circle) => {
+    circle.addEventListener('click', () => {
+        if (!(circle == active)) {
+            clearTimeout(alienTimeout);
+            stopGame();
+        }
+        else {
+            circle.style.backgroundImage = 'url("assets/images/planet1.png")';
+            if ((score != 0) && (score % 3 == 0)) {
+                interval -= 100;
+                console.log('decreasing interval!');
+            };
+            
+           
+        }
+        showScore(); 
+        return score;
     })
+   
+   
+})
+}
 
+/*///   SUBFUNCTIONS ////// */
 
+function activateAlien(){  //randomly selects active circle and applies alien background, initiates timeout
+    console.log('activating alien');
+    active = circles[newRandom()]; //global
+    console.log('active circle: ', active);
+        /*     circles[active].style.backgroundImage = getImage(); */
+    active.style.backgroundImage = 'url("assets/images/planet1_alien4.png")';
+ alienTimeout = setTimeout(activateAlien, 1500);
 }
 
 function finalScore(score) {
     gameOverText.textContent = `Your score is: ${score}.`;
-    if (score > 20) { finalScoreText.textContent = `Wowzers, you're better than Margit`; }
+    if (score > 20) { finalScoreText.textContent = `Wowzers, you're amazing`; }
     else if (score > 10) { finalScoreText.textContent = `Pretty good, for an amateur`; }
-    else { finalScoreText.textContent = `Man you suck.`; }
+    else { finalScoreText.textContent = `Honestly? Terrible.`; }
 }
 
 function generateRandom() {
@@ -69,11 +69,11 @@ function generateRandom() {
     return rand;
 }
 
-function verifyRandom() {
-    let newCurrent = generateRandom();
-    if (newCurrent != current) {
-        current = newCurrent;
-        return current;
+function newRandom() {
+    let newActive = generateRandom();
+    if (newActive != active) {
+        active = newActive;
+        return active;
     }
     else { generateRandom() }
 }
@@ -81,6 +81,8 @@ function verifyRandom() {
 function stopGame() {
     console.log('game ended.');
     clearTimeout(alienTimeout);
+    stopButton.style.display = "none";
+    startButton.style.display = "block";
     finalScore(score);
     showModal();
 }
@@ -119,7 +121,7 @@ function showModal() {
 
 /* //////////////  Event listeners   /////////////// */
 
-startButton.addEventListener('click', startGame);
+startButton.addEventListener('click', runGame);
 stopButton.addEventListener('click', stopGame);
 close.addEventListener('click', function () {
     overlay.classList.remove('visible');
