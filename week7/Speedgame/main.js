3f98d8b7c2e117fcb204159cabafe99a9bf3c72a
Anna Petelin <adpetelin@gmail.com>
2022-10-17 11:46:28 +0300

    tidy code in speedgame

--- week7/Speedgame/main.js
+++ week7/Speedgame/main.js
@@ -10,8 +10,9 @@ const overlay = document.querySelector(".overlay");
 const modal = document.querySelector(".modal");
 const gameOverText = document.querySelector("#gameOverText");
 const finalScoreText = document.querySelector("#finalScoreText");
-const defaultBG = 'url("assets/images/planet1.png")';
 const zap = new Audio("./assets/audio/zap_c_07-82067.mp3");
+const endSound1 = new Audio("./assets/audio/wrong-buzzer-6268.mp3");
+const endSound2 = new Audio("./assets/audio/game-over-38511.mp3");
 const endMusic = new Audio("./assets/audio/2018-08-01-30015.mp3");
 
 /* ////////////  GLOBAL VARIABLES //////////  */
@@ -25,8 +26,7 @@ let missedAliens = 0;
 
 function activateAlien() {
     /*randomly selects active circle and 
-  applies alien background, initiates timeout */
-    timer = 1800;
+  applies alien overlay, initiates timeout */
     newRandomIndex().then(() => {
         for (let i = 0; i < circles.length; i++) {
             setBG(i);
@@ -34,7 +34,6 @@ function activateAlien() {
     });
     alienTimeout = setTimeout(activateAlien, timer);
     missedAliens++;
-    console.log(missedAliens);
     if (missedAliens == 2) {
         stopGame();
     }
@@ -54,7 +53,6 @@ function setBG(index) {
         aliens[index].style.display = "inline-block";
         aliens[activeIndex].style.backgroundImage = getImage();
     } else {
-        circles[index].style.backgroundImage = defaultBG;
         aliens[index].style.display = "none";
     }
 }
@@ -63,10 +61,8 @@ function stopGame() {
     console.log("game ended.");
     clearTimeout(alienTimeout);
     circles.forEach((circle) => {
-        circle.style.backgroundImage = defaultBG;
         circle.style.pointerEvents = "none";
     });
-
     stopButton.style.display = "none";
     startButton.style.display = "block";
     finalScore(score);
@@ -80,8 +76,10 @@ function finalScore(score) {
         endMusic.play();
     } else if (score > 10) {
         finalScoreText.textContent = `Pretty good, for an amateur`;
+        endSound2.play();
     } else {
         finalScoreText.textContent = `Honestly? Terrible.`;
+        endSound1.play();
     }
 }
 
@@ -92,7 +90,6 @@ function getImage() {
 }
 
 function showModal() {
-    /* modal menu at game end */
     if (!overlay.classList.contains("visible")) {
         const close = document.querySelector(".close");
         overlay.classList.add("visible");
@@ -105,6 +102,7 @@ function runGame() {
     console.log("game started");
     score = 0;
     missedAliens = 0;
+    timer = 1800;
     circles.forEach((circle) => {
         circle.style.pointerEvents = "auto";
     });
@@ -126,7 +124,6 @@ close.addEventListener("click", function () {
     }
 });
 
-
 for (let i = 0; i < circles.length; i++) {
     circles[i].addEventListener("click", (event) => {
         zap.play();
@@ -136,7 +133,6 @@ for (let i = 0; i < circles.length; i++) {
         }
         else {
             missedAliens--;
-            console.log(missedAliens);
             score = score + 1;
             if (score != 0 && score % 4 == 0) {
                 timer -= 200;
@@ -144,6 +140,5 @@ for (let i = 0; i < circles.length; i++) {
             }
         }
         scoreDisplay.textContent = `Your score: ${score}`;
-
     });
 } 
