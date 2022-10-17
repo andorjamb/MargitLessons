69acd80c53cb5e510fcfa0355e1b6b63a5b804de
andor <andorjamb@protonmail.com>
2022-10-17 15:01:02 +0300

    update work on pokedex

--- pokedex/main.js
+++ pokedex/main.js
@@ -1,85 +1,127 @@
+/* /////////////   DOCUMENT OBJECTS   /////////////////  */ 
+
 const pokemonNumber = document.querySelector('#pokemon-number');
 const pokemonContainer = document.querySelector('.pokemon-container')
 const searchTerm = document.querySelector('#search-term');
 const submitSearch = document.querySelector('#submit-search');
 const genRadios = document.querySelectorAll('.gen-radio');
-let number = 10;
-let generation = 0;
-const genButtons = document.querySelectorAll('.gen');
+//const genButtons = document.querySelectorAll('.gen');
+
+
+/* ////////////////  GLOBAL VARIABLES    ////////////    */
+let amountPokemons = 10;
+let genNumber= 0;
 
 // example: 
 // https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0.
 //promise.all()
 //use map() method or forEach() to generate cards
 
+/*  ////////////  FUNCTIONS ///////////////   */
 
-function defineGen(generation) {
-    switch (generation) {
+function getGenNumber(){
+    genRadios.forEach((radio)=>{
+        if(radio.checked) {
+        console.log(radio.value);
+        genNumber = radio.value;
+        return genNumber;
+        
+    }
+}
+)
+} 
+
+function getGen(genNumber) {
+    switch (genNumber) {
         case "1":
-            return indexDivider = [151, 0];
+            return [151, 0];
             break;
         case "2":
-            return indexDivider = [100, 151];
+            return [100, 151];
             break;
         case "3":
-            return indexDivider = [135, 251];
+            return [135, 251];
             break;
         case "4":
-            return indexDivider = [107, 386];
+            return [107, 386];
             break;
         case "5":
-            return indexDivider = [156, 493];
+            return [156, 493];
             break;
         case "6":
-            return indexDivider = [72, 659];
+            return [72, 659];
             break;
         case "7":
-            return indexDivider = [88, 721];
+            return [88, 721];
             break;
         case "8":
-            return indexDivider = [96, 809];
+            return [96, 809];
             break;
-
     }
-
 }
 
-function pokemonDivMaker(number) {
-    for (let i = 0; i < number; i++) {
+function pokemonDivMaker(amountPokemons) {
+    for (let i = 0; i < amountPokemons; i++) {
         const pokemonDiv = document.createElement('div');
         pokemonContainer.appendChild(pokemonDiv);
         pokemonDiv.classList.add('card');
         const pokemonImage = document.createElement('img');
         pokemonDiv.appendChild(pokemonImage);
-
     }
 }
-
-function genWrapper(generation) {
++
+function genWrapper(g0, g1) {
     const baseURL = 'https://pokeapi.co/api/v2/pokemon';
-    fetchURL = '"' + baseURL + '?limit=' + defineGen()[0] + '&offet=' + defineGen()[1] + '"';
+    fetchURL = '"' + baseURL + '?limit=' + g0 + '&offset=' + g1+ '"';
     console.log(fetchURL);
     return fetchURL;
 
 }
-function APICall(term) {
-    let pokemonData = fetch(genWrapper()).then(function (response) { console.log('successful call'); return response.json() }).then(function (data) { console.log(data) }).catch(function (err) {
-        console.log('error', err);
+
+/* function searchPokemon(value) {
+   
+
+} */
+
+
+/*  /////////////////// RUNTIME   //////////////////////   */
+
+
+
+pokemonNumber.textContent = `There are ${amountPokemons} pokemons in generation ${genNumber}`
+pokemonDivMaker(amountPokemons);
+
+
+genRadios.forEach((radio)=>{
+    radio.addEventListener('change', ()=>{ 
+        getGenNumber();
+        let limit, offset;
+        [limit, offset] = getGen(genNumber);
+        console.log(limit, offset);
     })
 }
+)
 
-function searchPokemon(value) {
-    return APICall(value)
+genWrapper(limit, offset);
 
-}
 
-genRadios.forEach(genRadio => genRadio.addEventListener('click', function () {
-    console.log(genRadio.value);
-    defineGen(genRadio.value); //returns array
+/* const pokemonData = fetch(genWrapper(limit, offset))
+.then((response) => { 
+        response.json() 
+    })
+    .then(function (data) { 
+        console.log(data) }).catch(function (err) {
+            console.log('error', err);
+})
 
-}));
+ */
 
-pokemonDivMaker(10);
-console.log('index divider: ', indexDivider);
 
-pokemonNumber.textContent = `There are ${number} pokemons in generation ${generation}`
+
+/* const pokemonData = fetch(genWrapper(getGen()[0], getGen()[1])).then((response) => { 
+    response.json() }).then(function (data) { console.log(data) }).catch(function (err) {
+        console.log('error', err);
+    })
+ */
+
+//const pokemonDivs = pokemonData.map(pokemon => pokemon.)
