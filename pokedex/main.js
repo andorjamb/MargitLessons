057d7184babb5894f4db80c0f6324176da0b242d
Anna Petelin <andorjamb@protonmail.com>
2022-10-18 22:23:09 +0300

    switching to branch, earlier version of code

--- pokedex/main.js
+++ pokedex/main.js
@@ -1,120 +1,128 @@
 /* /////////////   DOCUMENT OBJECTS   /////////////////  */
 
-const pokemonNumber = document.querySelector('#pokemon-number');
-const pokemonContainer = document.querySelector('.pokemon-container')
-const searchTerm = document.querySelector('#search-term');
-const submitSearch = document.querySelector('#submit-search');
-const genRadios = document.querySelectorAll('.gen-radio');
-const nextPage = document.querySelector('#nextPage');
-const previousPage = document.querySelector('#previousPage');
+const pokemonNumber = document.querySelector("#pokemon-number");
+const pokemonContainer = document.querySelector(".pokemon-container");
+const searchTerm = document.querySelector("#search-term");
+const submitSearch = document.querySelector("#submit-search");
+const genRadios = document.querySelectorAll(".gen-radio");
+//const nextPage
+//const previousPage
 
 /* ////////////////  GLOBAL VARIABLES    ////////////    */
-const baseURL = 'https://pokeapi.co/api/v2/pokemon';
+const baseURL = "https://pokeapi.co/api/v2/pokemon";
+let genNumber = 0;
 let pokemons = [];
-let limit = 60;
-let offset = 1;
-let pokemon;
+let limit, offset;
 
+/*  //////////////// CLASS CONSTRUCTOR  ////////////////7   */
 
-/*  //////////////// CLASS CONSTRUCTOR  ////////////////   */
-
-class Pokemon  {
-    constructor ( id, pokeName, types, img) {
-        this.id = id;
-        this.name = pokeName;
-        this.types = types;
-        this.img = img;
-
-    }
+class Pokemon {
+  constructor(id, pokeName, types, img) {
+    this.id = id;
+    this.name = pokeName;
+    this.types = types;
+    this.img = img;
+  }
 }
 
 /*  ////////////  FUNCTIONS ///////////////   */
 
-
-function makePoke(data){ //parameter: object
-    let newPokemon = new Pokemon(data.id, data.name, data.types, data.img);
-    return newPokemon;
+function getGen(number) {
+  switch (number) {
+    case "1":
+      return [151, 0];
+      break;
+    case "2":
+      return [100, 151];
+      break;
+    case "3":
+      return [135, 251];
+      break;
+    case "4":
+      return [107, 386];
+      break;
+    case "5":
+      return [156, 493];
+      break;
+    case "6":
+      return [72, 659];
+      break;
+    case "7":
+      return [88, 721];
+      break;
+    case "8":
+      return [96, 809];
+      break;
+  }
 }
 
-function pokemonDivMaker(pokemons) {
-    pokemons.forEach((pokemon)=>{   
-        const pokemonDiv = document.createElement('div');
-    pokemonDiv.classList.add('card');
-    pokemonContainer.appendChild(pokemonDiv);
-    pokemonDiv.innerHTML = `
-    <img src="${pokemon.sprites.other['offical-artwork'].front_default}">
-    <p>Id: ${pokemon.id}</p>
-    <p>Name: ${pokemon.name}</p>
-    <p>Types: ${((pokemon.types).map((type)=>type.name)).join(", ")}</p>
-`}
- 
-    )
-    
-} 
-
-function searchPokemon() {
-    console.log('');
+function pokemonDivMaker(poke) {
+  const pokemonDiv = document.createElement("div");
+  pokemonDiv.classList.add("card");
+  pokemonContainer.appendChild(pokemonDiv);
+  pokemonDiv.innerHTML = `
+    <p>Name: ${poke.name}</p>
+    <p>ID: ${poke.id}</p>
+    `;
 }
 
-function getPokemons(offset, limit) {
-    /**
-     * maybe here: if (page == 2 ){offset = }
-     * else if(page == 3) etc
-     * but you would also need to do this for generations buttons?
-     */
-    pokemonContainer.innerHTML = ""; //sweep up
-for(let i=offset;i<limit;i++){
- let fetchURL = baseURL + `/${i}`;
-fetch(fetchURL)
-    .then(
-        response => response.json()
-    ).then(
-        (data) => {const newPoke = makePoke(data);
-            pokemons.push(newPoke);
-       console.log(newPoke);}  //an object pushed to an array of objects
-    )
-}  
-console.log(pokemons);
+function searchPokemon(term) {
+  if (pokemons.includes(term)) console.log("");
 }
 
-function getPokemonAttributes(pokemonArray) {
-    let requests = pokemonArray.map(pokemon => fetch(baseURL + `/${pokemon}`)
-    .then((response)=>response.json())
-    .then((data)=>{pokemonDivMaker(makeObject(data))})
-    )
-    // data returned from each request has been mapped to an object, 
-    // then fed to the divmaker
+function makeObject(data) {
+  let newPoke = new Pokemon(data.id, data.name, data.name, data.name);
+  console.log(newPoke);
+  return newPoke;
+}
+
+function getPokemons(limit, offset) {
+  pokemonContainer.innerHTML = "";
+  pokemons = [];
+  const fetchURL =
+    baseURL + "?offset=" + offset.toString() + "&limit=" + limit.toString();
+  fetch(fetchURL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      data.results.map((object) => {
+        pokemons.push(object.name);
+      });
+    });
+  return pokemons; // returning an indexed array of pokemon names for each generation
+}
+
+function getPokemonAttributes(arr) {
+  //[bulbasaur, ....etc]
+  let requests = arr.map((pokemon) =>
+    fetch(baseURL + `/${pokemon}`)
+      .then((response) => response.json())
+      .then((data) => {
+        return makeObject(data);
+      })
+  ); // data returned from each request has been mapped to an object
 
   Promise.all(requests);
-
+  /* for (const pokemon of pokemons) {
+    pokemonDivMaker(pokemon.name);
+}
+ */
 } //end of getPokemonAttributes
 
 /*  /////////////////// RUNTIME   //////////////////////   */
 
-//pre-populate some divs
-window.onload = getPokemons(offset,limit);
-
-nextPage.addEventListener('click', (page)=>{page++; getPokemons(offset, limit)});
-previousPage.addEventListener('click', (page)=>{page--; getPokemons(offset, limit)});
-
-//display pokemons by selected generation
-genRadios.forEach((radio) => {
-    radio.addEventListener('change', () => {
-        pokemons= [];
-        //[limit, offset] = getGen(radio.value);
-        pokemonNumber.textContent = `There are ${limit} pokemons in generation ${radio.value}`
-        getPokemonAttributes(getPokemons(limit, offset));
-    }
-    )
+searchTerm.addEventListener("submit", function () {
+  let searchWord = searchTerm.value;
+  searchPokemon(searchWord);
 });
 
-
-
-
-
-
-
-
-
-
-
+genRadios.forEach((radio) => {
+  radio.addEventListener("change", () => {
+    genNumber = radio.value;
+    pokemonArray = [];
+    [limit, offset] = getGen(genNumber);
+    pokemonNumber.textContent = `There are ${limit} pokemons in generation ${genNumber}`;
+    getPokemonAttributes(getPokemons(limit, offset));
+  });
+});
--- week7/Speedgame/main.js
+++ week7/Speedgame/main.js
@@ -21,95 +21,96 @@ let activeIndex = Math.floor(Math.random() * 4);
 let alienTimeout;
 let timer;
 let missedAliens = 0;
+let failsAllowed = 3;
 
 /* ////////////  FUNCTION DEFINITIONS //////////  */
 
 function activateAlien() {
-    /*randomly selects active circle and 
+  /*randomly selects active circle and 
   applies alien overlay, initiates timeout */
-    newRandomIndex().then(() => {
-        for (let i = 0; i < circles.length; i++) {
-            setBG(i);
-        }
-    });
-    alienTimeout = setTimeout(activateAlien, timer);
-    missedAliens++;
-    if (missedAliens > 2) {
-        stopGame();
+  newRandomIndex().then(() => {
+    for (let i = 0; i < circles.length; i++) {
+      setBG(i);
     }
+  });
+  alienTimeout = setTimeout(activateAlien, timer);
+  missedAliens++;
+  if (missedAliens > failsAllowed) {
+    stopGame();
+  }
 }
 
 async function newRandomIndex() {
-    let rand = Math.floor(Math.random() * 4);
-    if (!(rand === activeIndex)) {
-        activeIndex = rand;
-        return activeIndex;
-    } else newRandomIndex();
+  let rand = Math.floor(Math.random() * 4);
+  if (!(rand === activeIndex)) {
+    activeIndex = rand;
+    return activeIndex;
+  } else newRandomIndex();
 }
 
 function setBG(index) {
-    /** sets background image for circles */
-    if (index == activeIndex) {
-        aliens[index].style.display = "inline-block";
-        aliens[activeIndex].style.backgroundImage = getImage();
-    } else {
-        aliens[index].style.display = "none";
-    }
+  /** sets background image for circles */
+  if (index == activeIndex) {
+    aliens[index].style.display = "inline-block";
+    aliens[activeIndex].style.backgroundImage = getImage();
+  } else {
+    aliens[index].style.display = "none";
+  }
 }
 
 function stopGame() {
-    console.log("game ended.");
-    clearTimeout(alienTimeout);
-    circles.forEach((circle) => {
-        circle.style.pointerEvents = "none";
-    });
-    stopButton.style.display = "none";
-    startButton.style.display = "block";
-    finalScore(score);
-    showModal();
+  console.log("game ended.");
+  clearTimeout(alienTimeout);
+  circles.forEach((circle) => {
+    circle.style.pointerEvents = "none";
+  });
+  stopButton.style.display = "none";
+  startButton.style.display = "block";
+  finalScore(score);
+  showModal();
 }
 
 function finalScore(score) {
-    gameOverText.textContent = `Your score is: ${score}.`;
-    if (score > 20) {
-        finalScoreText.textContent = `Wowzers, you're amazing`;
-        endMusic.play();
-    } else if (score > 10) {
-        finalScoreText.textContent = `Pretty good, for an amateur`;
-        endSound2.play();
-    } else {
-        finalScoreText.textContent = `Honestly? Terrible.`;
-        endSound1.play();
-    }
+  gameOverText.textContent = `Your score is: ${score}.`;
+  if (score > 20) {
+    finalScoreText.textContent = `Wowzers, you're amazing`;
+    endMusic.play();
+  } else if (score > 10) {
+    finalScoreText.textContent = `Pretty good, for an amateur`;
+    endSound2.play();
+  } else {
+    finalScoreText.textContent = `Honestly? Terrible.`;
+    endSound1.play();
+  }
 }
 
 function getImage() {
-    let imageURLroot = 'url("./assets/images/planet1_alien';
-    imageURL = imageURLroot + Math.ceil(Math.random() * 4).toString() + '.png")';
-    return imageURL;
+  let imageURLroot = 'url("./assets/images/planet1_alien';
+  imageURL = imageURLroot + Math.ceil(Math.random() * 4).toString() + '.png")';
+  return imageURL;
 }
 
 function showModal() {
-    if (!overlay.classList.contains("visible")) {
-        const close = document.querySelector(".close");
-        overlay.classList.add("visible");
-        modal.classList.add("visible");
-        close.classList.add("visible");
-    }
+  if (!overlay.classList.contains("visible")) {
+    const close = document.querySelector(".close");
+    overlay.classList.add("visible");
+    modal.classList.add("visible");
+    close.classList.add("visible");
+  }
 }
 
 function runGame() {
-    console.log("game started");
-    score = 0;
-    missedAliens = 0;
-    timer = 1800;
-    circles.forEach((circle) => {
-        circle.style.pointerEvents = "auto";
-    });
-    scoreDisplay.textContent = `Your score: ${score}`;
-    startButton.style.display = "none";
-    stopButton.style.display = "block";
-    activateAlien();
+  console.log("game started");
+  score = 0;
+  missedAliens = 0;
+  timer = 2400;
+  circles.forEach((circle) => {
+    circle.style.pointerEvents = "auto";
+  });
+  scoreDisplay.textContent = `Your score: ${score}`;
+  startButton.style.display = "none";
+  stopButton.style.display = "block";
+  activateAlien();
 }
 
 /* //////////////  Event listeners   /////////////// */
@@ -117,28 +118,27 @@ function runGame() {
 startButton.addEventListener("click", runGame);
 stopButton.addEventListener("click", stopGame);
 close.addEventListener("click", function () {
-    overlay.classList.remove("visible");
-    if (endMusic.play()) {
-        endMusic.pause();
-        endMusic.currentTime = 0;
-    }
+  overlay.classList.remove("visible");
+  if (endMusic.play()) {
+    endMusic.pause();
+    endMusic.currentTime = 0;
+  }
 });
 
 for (let i = 0; i < circles.length; i++) {
-    circles[i].addEventListener("click", (event) => {
-        zap.play();
-        if (!(i == activeIndex)) {
-            clearTimeout(alienTimeout);
-            stopGame();
-        }
-        else {
-            missedAliens--;
-            score = score + 1;
-            if (score != 0 && score % 4 == 0) {
-                timer -= 200;
-                console.log("decreasing interval to", timer);
-            }
-        }
-        scoreDisplay.textContent = `Your score: ${score}`;
-    });
-} 
+  circles[i].addEventListener("click", (event) => {
+    zap.play();
+    if (!(i == activeIndex)) {
+      clearTimeout(alienTimeout);
+      stopGame();
+    } else {
+      missedAliens--;
+      score = score + 1;
+      if (score != 0 && score % 4 == 0) {
+        timer -= 200;
+        console.log("decreasing interval to", timer);
+      }
+    }
+    scoreDisplay.textContent = `Your score: ${score}`;
+  });
+}
