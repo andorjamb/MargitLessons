d9afd7d436d7aae9dae15090c3c9adcc969135d4
andor <andorjamb@protonmail.com>
2022-10-28 17:38:00 +0300

    fix pokedex search all cases, add mobile button

--- pokedex/main.js
+++ pokedex/main.js
@@ -7,6 +7,9 @@ const form = document.querySelector("form");
 const searchTerm = document.querySelector("#search-term");
 const genRadios = document.querySelectorAll(".gen-radio");
 const nextSixty = document.querySelector("#nextSixty");
+const menuIcon = document.querySelector(".menu-icon");
+const buttonContainer = document.querySelector(".button-container");
+let gen = document.querySelectorAll('.gen');
 
 /* ////////////////  GLOBAL VARIABLES    ////////////    */
 const baseURL = "https://pokeapi.co/api/v2/pokemon";
@@ -15,7 +18,7 @@ let pokemons = [];
 let attributes = [];
 let limit = "60";
 let offset = "0";
-let pageNo = 0;
+let pageNo = 1;
 
 /*  //////////////// CLASS CONSTRUCTOR  ////////////////7   */
 
@@ -131,7 +134,7 @@ function searchPokemon(term) {
 window.onload = getPokemons(limit, offset);
 setTimeout(() => {
   getAttributes(getPokemons);
-}, 1000);
+}, 500);
 
 nextSixty.addEventListener("click", function () {
   pageNo = Number(pageNo) + 1;
@@ -139,6 +142,7 @@ nextSixty.addEventListener("click", function () {
     nextSixty.style.pointerEvents = "none";
     return 0;
   } else {
+    /** This still needs attention  */
     offset = (Number(pageNo) * 60).toString();
     pokemonNumber.textContent = `Showing up to pokemon ${Number(offset)}`;
     limit = "60";
@@ -148,12 +152,12 @@ nextSixty.addEventListener("click", function () {
   }
 });
 
+menuIcon.addEventListener('click', ()=>{buttonContainer.classList.add(".mobile"); gen.classList.add(".mobile")});
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   pokemonNumber.textContent = "";
-  console.log(searchTerm);
-  console.log(searchTerm.value);
-  searchPokemon(searchTerm.value);
+  searchPokemon(searchTerm.value.toLowerCase());
 });
 
 genRadios.forEach((radio) => {
